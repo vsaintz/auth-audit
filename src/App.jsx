3383134b7ce7fdc4e248352621d3c96b96e1680d
vsaintz <roland.lepcha.official@gmail.com>
2025-01-22 17:06:01 +0530

    | FILE |

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,25 @@
+import React from "react";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+
+import Navigation from "./components/Navigation";
+import Index from "./components/Index.jsx";
+import LoginPage from "./components/LoginPage.jsx";
+import SignUpPage from "./components/SignUpPage.jsx";
+
 function App() {
 
   return (
     <>
-      <div className="h-screen bg-gray-600 text-white flex items-center justify-center text-1xl">
-        Tailwind CSS is working!
-      </div>
-
+      <Router>
+        <Navigation />
+        <Routes>
+          <Route path="/" element={<Index />} />
+          <Route path="/login" element={<LoginPage />} />
+          <Route path="/signup" element={<SignUpPage />} />
+        </Routes>
+      </Router>
     </>
-  )
-}
+  );
+};
 
-export default App
+export default App;
