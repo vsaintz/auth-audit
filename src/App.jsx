9e168c9828d55c5166a6ea1928f3c25a7aa4ede0
vsaintz <roland.lepcha.official@gmail.com>
2025-01-22 15:08:28 +0530

    | FILE |

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+function App() {
+
+  return (
+    <>
+      <div className="h-screen bg-gray-600 text-white flex items-center justify-center text-1xl">
+        Tailwind CSS is working!
+      </div>
+
+    </>
+  )
+}
+
+export default App
