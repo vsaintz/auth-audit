2903b6c91fde732ecdc2cbfe707b2a6c8361b336
vsaintz <roland.lepcha.official@gmail.com>
2025-01-23 17:32:00 +0530

    code: refactor of code for landing page

--- src/components/Index.jsx
+++ src/components/Index.jsx
@@ -1,6 +1,6 @@
 const Index = () => {
     return (
-        <div className="h-screen bg-blue-500 text-white flex items-center justify-center text-1xl">
+        <div className="h-screen bg-indigo-700 text-white flex items-center justify-center text-1xl">
             Tailwind CSS is working!
         </div>
     );
