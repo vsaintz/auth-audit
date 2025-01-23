63bc4d342a4d368665561ec1a4b93550ec6b7e73
vsaintz <roland.lepcha.official@gmail.com>
2025-01-23 17:35:00 +0530

    code: naviagation page code refactor

--- src/components/Navigation.jsx
+++ src/components/Navigation.jsx
@@ -2,18 +2,16 @@ import { Link } from 'react-router-dom'
 
 const Navigation = () => {
     return (
-        <div className="border-b-2 flex items-center justify-between w-full p-4 absolute top-0 left-0">
-            {/* Centered third link */}
+        <div className="flex items-center justify-between w-full p-4 absolute top-0 left-0">
             <h1 className="text-white text-2xl">Third Link</h1>
 
-            {/* Right-aligned login and signup */}
             <div>
                 <ul className="text-white flex gap-10">
                     <li>
-                        <Link to='/login'> Login </Link>
+                        <Link to='/signin'> SignIn </Link>
                     </li>
                     <li>
-                        <Link to="/signup"> Sign Up </Link>
+                        <Link to="/signup"> SignUp </Link>
                     </li>
                 </ul>
             </div>
