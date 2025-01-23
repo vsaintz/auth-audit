ca54444b154007f3d84c5215142bc0a06f5dc0ca
vsaintz <roland.lepcha.official@gmail.com>
2025-01-23 21:04:58 +0530

    | FILE |

--- README.md
+++ README.md
@@ -1,8 +1,4 @@
-# React + Vite
 
-This template provides a minimal setup to get React working in Vite with HMR and some ESLint rules.
-
-Currently, two official plugins are available:
-
-- [@vitejs/plugin-react](https://github.com/vitejs/vite-plugin-react/blob/main/packages/plugin-react/README.md) uses [Babel](https://babeljs.io/) for Fast Refresh
-- [@vitejs/plugin-react-swc](https://github.com/vitejs/vite-plugin-react-swc) uses [SWC](https://swc.rs/) for Fast Refresh
+```plaintext
+This software if used to check or validate all the third party application or services connected to your google account.
+```
--- src/components/Index.jsx
+++ src/components/Index.jsx
@@ -1,8 +1,19 @@
 const Index = () => {
     return (
-        <div className="h-screen bg-indigo-700 text-white flex items-center justify-center text-1xl">
-            Tailwind CSS is working!
+        <div className="h-screen bg-gray-900 text-white flex items-center justify-center text-1xl">
+            <div className="flex flex-col justify-center items-center w-xs">
+                <h1 className="text-5xl m-3">Third Link</h1>
+                <p className="w-full max-w-full p-3 text-center">
+                    Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quasi dolorem quidem,
+                    sunt iste quibusdam atque vero aut sapiente provident odio maiores harum ipsa,
+                    aspernatur iure ipsam nemo, veritatis animi possimus?
+                    Lorem ipsum dolor sit amet consectetur adipisicing elit. Ullam nulla facere molestias
+                    officiis beatae iure quis! Sequi amet repellat quisquam est nisi temporibus aut,
+                    obcaecati quam. Voluptatum cum similique repellat!
+                </p>
+            </div>
         </div>
     );
 };
+
 export default Index;
